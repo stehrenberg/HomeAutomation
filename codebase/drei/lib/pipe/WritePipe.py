45b3975ec151d21613bcc2e16d0a83741f0ff11d
ifs11065 <ifs11065@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-09 10:33:59 +0000

    IPK

--- codebase/drei/lib/pipe/WritePipe.py
+++ codebase/drei/lib/pipe/WritePipe.py
@@ -4,11 +4,13 @@ __author__ = 'markus'
 class WritePipe:
 
     def __init__(self, filename):
-        """
+        """ Open file for pipe
         """
         self.pipe = open(filename, 'w')
 
     def notify(self, content):
+        """ Write MAC-Address in pipe
+        """
         self.pipe.write(content)
 
     def close(self):
--- codebase/drei/test/lib/pipe/PipeTest.py
+++ codebase/drei/test/lib/pipe/PipeTest.py
@@ -1,12 +1,16 @@
 from lib.pipe.ReadPipe import ReadPipe
 from lib.pipe.WritePipe import WritePipe
+from unittest.mock import Mock
 
 __author__ = 'markus'
 
+import time
 import unittest
 
 
 class PipeTest(unittest.TestCase):
+    first_test = False
+    second_test = False
 
     def setUp(self):
         self.write_pipe = WritePipe("./testpipe")
@@ -15,16 +19,22 @@ class PipeTest(unittest.TestCase):
 
     def example_function_positive(self, content):
         self.assertEqual(content, self.test_string)
+        PipeTest.first_test = True
         print("blub")
 
     def example_function_negative(self, content):
         self.assertNotEqual(content, "bla")
+        PipeTest.second_test = True
         print("bla")
         self.read_pipe.close()
 
     def test_read_write(self):
-        self.read_pipe.register(self.example_function_positive)
-        self.read_pipe.register(self.example_function_negative)
+        funktion1 = Mock()
+        self.read_pipe.register(funktion1)
+        """self.read_pipe.register(self.example_function_negative)"""
 
         self.write_pipe.notify(self.test_string)
+        funktion1.assert_called_with(self.test_string)
+
+        time.sleep(1)
         self.write_pipe.close()

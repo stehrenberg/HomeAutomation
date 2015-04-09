4d1ae9b7d3315c112a30ca4a9d7c018b8ad199ee
ifs11030 <ifs11030@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-09 20:42:19 +0000

    Finished first implementation of the RESTful webservice which serves CRUD operations.

--- codebase/drei/webserver/RestEndpoint.py
+++ codebase/drei/webserver/RestEndpoint.py
@@ -1,12 +1,12 @@
 import json
 import logging
 
-from flask import Flask, request, abort, jsonify
-
+from flask import Flask, request, abort
 from flask.ext.socketio import SocketIO
 
 from database.database import MockDatabase
 from dto.user import User
+
 
 __author__ = 's.jahreiss'
 
@@ -17,31 +17,33 @@ db = MockDatabase()
 
 @app.route('/api/users', methods=['GET'])
 def get_users():
+    # Serialize users
     users = json.dumps([user.__dict__ for user in db.retrieve_users()])
-    return users
+    return users, 200
 
 
 @app.route('/api/users', methods=['POST'])
 def add_user():
-    if not request.json or not 'user' in request.json:
+    if not request.json:
         abort(400)
-    user = parse_user(request.json['user'])
-    return jsonify({'user': db.add_user(user)}), 201
+    user = parse_user(request.json)
+    created = db.add_user(user)
+    return serialize_boolean_response('created', created), 201
 
 
-@app.route('/api/users/<int:user_id>', methods=['PUT'])
+@app.route('/api/users/<string:user_id>', methods=['PUT'])
 def update_user(user_id):
-    if not request.json or not 'user' in request.json:
+    if not request.json:
         abort(400)
-    user = parse_user(request.json['user'])
-    user = db.update_user(user_id, user)
-    return {'user': user}
+    user = parse_user(request.json)
+    updated = db.update_user(user_id, user)
+    return serialize_boolean_response('updated', updated), 202
 
 
-@app.route('/api/users/<int:user_id>', methods=['DELETE'])
+@app.route('/api/users/<string:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    db.delete_user(user_id)
-    return {'Result': True}
+    deleted = db.delete_user(user_id)
+    return serialize_boolean_response('deleted', deleted), 200
 
 
 def parse_user(user_json):
@@ -50,6 +52,10 @@ def parse_user(user_json):
     sound = user_json["sound"]
     light = user_json["light"]
     return User(mac, name, sound, light)
+
+
+def serialize_boolean_response(key, value):
+    return json.dumps({key: value})
 
 
 def start_rest_endpoint():

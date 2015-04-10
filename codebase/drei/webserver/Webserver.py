7e3b4762211252e17f1a9984386914228b3d8fe7
ifs11030 <ifs11030@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-10 09:46:35 +0000

    Added some comments.

--- codebase/drei/webserver/Webserver.py
+++ codebase/drei/webserver/Webserver.py
@@ -10,11 +10,23 @@ __author__ = 's.jahreiss'
 
 
 class Webserver(multiprocessing.Process):
+    """
+    Represents the webserver. It starts a RESTful and a websocket service.
+    """
+
     def __init__(self, manager_queue):
+        """
+        Constructor of the webserver.
+        :param manager_queue: The queue which will be used to retrieve messages from the manager.
+        :return: A new webserver.
+        """
         multiprocessing.Process.__init__(self)
         self.manager_queue = manager_queue
 
     def run(self):
+        """
+        Starts the webserver.
+        """
         print("Webserver: Running on http://127.0.0.1:8080")
 
         start_new_thread(notify, ())

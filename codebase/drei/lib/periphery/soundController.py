a0105ac6d6160ac6b49d5d03b446c674d7b5df31
ifw12385 <ifw12385@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-16 08:24:49 +0000

    The SoundController runs in an own thread

--- codebase/drei/lib/periphery/soundController.py
+++ codebase/drei/lib/periphery/soundController.py
@@ -1,30 +1,64 @@
 __author__ = 'Luis'
 
 import pygame
+from threading import Thread
 
 
-class SoundController(object):
+class SoundController(Thread):
+    """The sound controller handle the"""
+
     def __init__(self):
-        # TODO Put it in a thread
+        Thread.__init__(self)
+        self.running = 0
         pygame.init()
 
-    def load(self, songTitle):
-        self.song = pygame.mixer.music.load(songTitle)
+    def load(self, song_title):
+        """Load a new song.
+            If a new song is loaded, the actual is stopped."""
+
+        self.running = 0
+        pygame.mixer.music.load(song_title)
 
     def play(self):
+        """Play the song, as long as its not stopped."""
 
-        try:
-            clock = pygame.time.Clock()
+        self.start()
 
-            pygame.mixer.music.play()
+    def run(self):
+        self.running = 1
 
-            while True:
-                clock.tick(60)
+        clock = pygame.time.Clock()
 
-        except KeyboardInterrupt:
-            print "sound: KeyboardInterrupt"
-            self.stop()
+        pygame.mixer.music.play()
+
+        while self.running:
+            clock.tick(60)
+
+    def pause(self):
+        """Pause the actual song."""
+
+        self.running = 0
+
+    def stop(self):
+        """Stop the song and close the instance."""
+
+        self.pause()
+        pygame.quit()
+        self.join(2)
 
 
-def stop(self):
-    pygame.quit()
+if __name__ == '__main__':
+    from time import sleep
+
+    SONG = 'soundFiles/Knight-Rider-Theme-Song.mp3'
+
+    sound = SoundController()
+    sound.load(SONG)
+    sound.play()
+
+    print "Song started"
+
+    sleep(30)
+    sound.stop()
+
+    print "Song stopped"

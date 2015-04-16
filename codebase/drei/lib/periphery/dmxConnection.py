4206abd877d68460036ff68f02c2f07f0a6c7cb5
ifw12385 <ifw12385@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-16 08:11:32 +0000

    Added support for the DMX address Added PortNotOpenException, InvalidChannelException and NoConnectionException

--- codebase/drei/lib/periphery/dmxConnection.py
+++ codebase/drei/lib/periphery/dmxConnection.py
@@ -1,66 +1,92 @@
-__author__ = 'Uni'
+__author__ = 'Luis'
 
-import sys, serial
+import serial
+
+
+#############################
+#  Protocol configurations  #
+#############################
 
 START_VAL = 0x7E
 END_VAL = 0xE7
+
+MIN_VAL = 0
+MAX_VAL = 255
+
+MIN_CHANNELS = 1
+MAX_CHANNELS = 512
 
 COM_BAUD = 57600
 COM_TIMEOUT = 1
 COM_PORT = 7
 
-LABELS = {
-    'GET_WIDGET_PARAMETERS': 3,  # unused
-    'SET_WIDGET_PARAMETERS': 4,  #unused
-    'RX_DMX_PACKET': 5,  #unused
-    'TX_DMX_PACKET': 6,
-    'TX_RDM_PACKET_REQUEST': 7,  #unused
-    'RX_DMX_ON_CHANGE': 8,  #unused
-}
+LABELS = {'GET_WIDGET_PARAMETERS': 3,   # unused
+          'SET_WIDGET_PARAMETERS': 4,   # unused
+          'RX_DMX_PACKET': 5,           # unused
+          'TX_DMX_PACKET': 6,
+          'TX_RDM_PACKET_REQUEST': 7,   # unused
+          'RX_DMX_ON_CHANGE': 8,        # unused
+          }
 
 
-class DMXConnection(object):
-    def __init__(self, comport=None):
+class DMXConnection(object):            # Could be replaced with a singleton
+    """DMXConnection represents the connection to a single DMX device.
+        The DMX address can be set."""
+
+    def __init__(self, device, dmx_address=0):
 
         self.com = 0
         self.dmx_frame = list()
+        self.dmx_address = dmx_address
 
         # setup channel output list
-        for i in xrange(513):
-            self.dmx_frame.append(0)
+        for i in xrange(MAX_CHANNELS + 1):
+            self.dmx_frame.append(MIN_VAL)
 
         try:
-            self.com = serial.Serial(comport, baudrate=COM_BAUD, timeout=COM_TIMEOUT)
+            self.com = serial.Serial(device, baudrate=COM_BAUD, timeout=COM_TIMEOUT)
         except:
-            print "Could not open %s, quitting application" % (comport)
-            sys.exit(0)
+            print "Could not open %s, quitting application" % device
+            raise PortNotOpenException
 
-        print "Opened %s" % (self.com.portstr)
+        print "Opened %s" % self.com.portstr
 
-    def setChannel(self, chan, val, autorender=False):
-        # takes channel and value arguments to set a channel level in the local
+    def set_channel(self, channel, val, auto_render=False):
+        """Set the channel to a specified value.
+            You can select if the should"""
+
+        #  takes channel and value arguments to set a channel level in the local
         #  dmx frame, to be rendered the next time the render() method is called
-        if (chan > 512) or (chan < 1):
-            print "invalid channel"
-            return
-        if val > 255: val = 255
-        if val < 0: val = 0
-        self.dmx_frame[chan] = val
-        if autorender:
+        if (channel > MAX_CHANNELS) or (channel < MIN_CHANNELS):
+            print "Invalid channel"
+            raise InvalidChannelException
+
+        if val > MAX_VAL:
+            val = MAX_VAL
+
+        if val < MIN_VAL:
+            val = MIN_VAL
+
+        self.dmx_frame[self.dmx_address + channel] = val
+
+        if auto_render:
             self.render()
 
-    def clear(self, chan=0):
-        # clears all channels to zero. blackout.
-        #  with optional channel argument, clears only one channel
-        if chan == 0:
-            for i in xrange(1, 512, 1):
-                self.dmx_frame[i] = 0
+    def clear(self, channel=0):
+        """Clear all channels.
+            Set channel to clear only the specified channel."""
+
+        if channel == 0:
+            for i in xrange(MIN_CHANNELS, MAX_CHANNELS):
+                self.dmx_frame[i] = MIN_VAL
         else:
-            self.dmx_frame[chan] = 0
+            self.dmx_frame[channel] = MIN_VAL
 
     def render(self):
-        # updates the dmx output from the USB DMX Pro with the values from self.dmx_frame
-        packet = []
+        """Set all the pixel that were set before."""
+
+        #  updates the dmx output from the USB DMX Pro with the values from self.dmx_frame
+        packet = list()
         packet.append(chr(START_VAL))
         packet.append(chr(LABELS['TX_DMX_PACKET']))
         packet.append(chr(len(self.dmx_frame) & 0xFF))
@@ -74,4 +100,27 @@ class DMXConnection(object):
         self.com.write(''.join(packet))
 
     def close(self):
+        """Close the port."""
+
         self.com.close()
+
+
+class PortNotOpenException(Exception):
+    """Exception is raised when the port couldn't be open."""
+
+    def __init__(self, arg):
+        self.arg = arg
+
+
+class InvalidChannelException(Exception):
+    """Exception is raised when you try to assign a channel outside of the range."""
+
+    def __init__(self, arg):
+        self.arg = arg
+
+
+class NoConnectionException(Exception):
+    """The Exception is raised when the device is not plugged in."""
+
+    def __init__(self, arg):
+        self.arg = arg

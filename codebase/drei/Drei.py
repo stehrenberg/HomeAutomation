847a01cf58369522f8ab6a4b75c8062cac01f3f7
ifs11030 <ifs11030@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-16 12:55:56 +0000

    Added correct drei app.

--- codebase/drei/Drei.py
+++ codebase/drei/Drei.py
@@ -18,20 +18,20 @@ if __name__ == '__main__':
     manager_webserver_queue = multiprocessing.Queue()
 
     # Start the crawler
-    # crawler = Crawler(crawler_manager_queue)
-    # crawler.start()
+    crawler = Crawler(crawler_manager_queue)
+    crawler.start()
 
     # Start the manager
-    # manager = Manager(crawler_manager_queue, manager_webserver_queue)
-    # manager.start()
+    manager = Manager(crawler_manager_queue, manager_webserver_queue)
+    manager.start()
 
     # Start the webserver
     webserver = Webserver(manager_webserver_queue)
     webserver.start()
 
     # Wait until both processes end
-    # crawler_manager_queue.close()
-    # crawler_manager_queue.join_thread()
-    # crawler.join()
-    # manager.join()
+    crawler_manager_queue.close()
+    crawler_manager_queue.join_thread()
+    crawler.join()
+    manager.join()
     webserver.join()

6541299d908c2cbe44fcd45894a9edac438c6f7e
ifw12078 <ifw12078@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-16 09:19:42 +0000

    merged dummy crawler and wifi crawler

--- codebase/drei/crawler/Crawler.py
+++ codebase/drei/crawler/Crawler.py
@@ -1,24 +1,51 @@
 import multiprocessing
 import time
+import re
+import subprocess
 
 __author__ = 's.jahreiss'
 
 
 class Crawler(multiprocessing.Process):
-    def __init__(self, manager_queue):
+
+    def __init__(self, manager_queue, dummy=True):
         multiprocessing.Process.__init__(self)
         self.manager_queue = manager_queue
+        self.dummy = dummy
 
     def run(self):
         print("Crawler: Running")
 
-        # TODO: add logic
+        # dummy mode, send addresses in random time slices
+        if self.dummy:
+            while True:
+                time.sleep(2)
+                self.manager_queue.put(['online', '00:80:41:ae:fd:7e'])
+                print("New peer connected: ", '00:80:41:ae:fd:7e')
+                time.sleep(2)
+                self.manager_queue.put(['offline', '00:80:41:ae:fd:7e'])
+                print("Peer disconnected: ", '00:80:41:ae:fd:7e')
 
-        while True:
-            time.sleep(2)
-            self.manager_queue.put(['a1', 'a2'])
-            time.sleep(2)
-            self.manager_queue.put(['a1', 'b2'])
-            time.sleep(2)
-            self.manager_queue.put(['b2', 'a3'])
-            # print("Crawler: Sent some addresses")
+        # listen of events from WIFI interface
+        else:
+            # regex pattern for iwevent messages
+            pattern = re.compile('(.*?)\s+(.*?)\s+([\w|\s]+):(.*)$')
+
+            # spaw new iwevent process and capture output
+            result = subprocess.Popen("iwevent", shell=True, stdout=subprocess.PIPE)
+
+            # each line is a new event
+            for line in result.stdout:
+                matcher = pattern.search(line.decode("utf-8"))
+                if matcher:
+                    # new peer connected
+                    if "Registered" in matcher.group(3):
+                        print("New peer connected: " + matcher.group(4))
+                        self.manager_queue.put(['online', matcher.group(4)])
+
+                    # peer disconnected
+                    elif "Expired" in matcher.group(3):
+                        print("Peer disconnected: " + matcher.group(4))
+                        self.manager_queue.put(['offline', matcher.group(4)])
+
+        print("Crawler exited")

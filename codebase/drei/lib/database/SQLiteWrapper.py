52760890ee36e21c200e934621d376c4280b1a4e
ifw12025 <ifw12025@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-29 17:55:17 +0000

    return_val fuer add_user() eingebaut, retrieve_users() liefert jetzt User-Obj-Liste, list_songs() liefert Liste an Songs

--- codebase/drei/lib/database/SQLiteWrapper.py
+++ codebase/drei/lib/database/SQLiteWrapper.py
@@ -7,7 +7,7 @@ __author__ = 's.ehrenberg'
 
 
 class SQLiteWrapper(Database):
-    """ Simple interface for the database.
+    """ Provides a connection to the SQLite Database.
     """
 
     def __init__(self, db="test.db"):
@@ -18,31 +18,38 @@ class SQLiteWrapper(Database):
         """
         Adds a user to the database.
         :param user: The user who will be added.
+        :returns true, if adding was successful, otherwise false.
         """
+        wasAddedSuccessful = False
+
         with sql.connect(self.db) as db_con:
+
             cursor = db_con.cursor()
+
             if not self.user_exists(cursor, user.user_mac):
-                cursor.execute("""
-                    INSERT INTO Lights(light_address, light_color)
-                    VALUES(?, ?);""", (user.light_address, user.light_color))
-                cursor.execute("""SELECT light_ID FROM Lights WHERE rowid=?;""", cursor.lastrowid)
-                light_ID = cursor.fetchone()
+
                 path_chunks = user.sound.split('/')
                 sound_title = path_chunks[len(path_chunks) - 1]
+
                 cursor.execute("""
                     INSERT INTO Sounds(title, filepath)
                     VALUES(?, ?);""", (sound_title, user.sound))
                 cursor.execute("""SELECT sound_ID FROM Sounds WHERE rowid=?;""", cursor.lastrowid)
-                sound_ID = cursor.fetchone()
+                sound_ID = cursor.fetchone()[0] # TODO index evtl. nicht noetig, testen!
+
                 cursor.execute("""
-                    INSERT INTO Users(name, mac_address, sound, light)
+                    INSERT INTO Users(name, mac_address, sound, light_color)
                     VALUES(?, ?, ?, ?);""", (
                     user.user_mac,
                     user.user_name,
                     sound_ID,
-                    light_ID))
+                    user.light_color))
+                wasAddedSuccessful = True
+
             else:
                 print("User already exists. Nothing inserted.")
+
+            return wasAddedSuccessful
 
 
     def get_user(self, user_mac):
@@ -50,15 +57,24 @@ class SQLiteWrapper(Database):
         Returns the user with a given MAC address.
         :return: A user object.
         """
+
+        user = None
+
         with sql.connect(self.db) as db_con:
             cursor = db_con.cursor()
+
             cursor.execute("SELECT * FROM Users WHERE mac_address=?", user_mac)
             mac_add, name, sound, light = cursor.fetchall()
+
             cursor.execute("SELECT * FROM Sounds WHERE sound_ID=?", sound)
             sound_ID, title, filepath = cursor.fetchone()
+
             cursor.execute("SELECT * FROM Lights WHERE light_ID=?", light)
             light_ID, light_address, light_color = cursor.fetchone()
-            return User(mac_add, name, filepath, light_ID, light_color)
+
+            user = User(mac_add, name, filepath, light_ID, light_color)
+
+        return user
 
 
     def retrieve_users(self):
@@ -66,11 +82,17 @@ class SQLiteWrapper(Database):
         Returns a list with all users.
         :return: A list containing all users.
         """
+        mac_addresses = []
+        users = []
+
         with sql.connect(self.db) as db_con:
             cursor = db_con.cursor()
-            cursor.execute("SELECT mac_address, username FROM Users")
-            data = cursor.fetchall()
-            # create_user_list(data)
+            cursor.execute("SELECT mac_address FROM Users")
+            mac_addresses = cursor.fetchall()
+
+        for mac_add in mac_addresses:
+            user = self.get_user(mac_add)
+            users.append(user)
 
 
     def update_user(self, user_id, user):
@@ -95,3 +117,18 @@ class SQLiteWrapper(Database):
             return False
         else:
             return True
+
+
+    def list_sounds(self):
+        """
+        Provides a list of all sounds.
+        :return: A list, containing the tuples (title, path).
+        """
+        sound_list = []
+        with sql.connect(self.db) as db_con:
+            cursor = db_con.cursor()
+            cursor.execute("SELECT * FROM Sounds")
+            sound_list = cursor.fetchall()
+
+        return sound_list
+

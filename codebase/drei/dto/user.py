e37e9db455bee35ca8c7123760dd48265791515f
ifw12025 <ifw12025@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-04-30 17:29:53 +0000

    toString-Äquivalente eingebaut

--- codebase/drei/dto/user.py
+++ codebase/drei/dto/user.py
@@ -11,3 +11,17 @@ class User:
         self.mac = user_mac
         self.light_id = user_light_id
         self.light_color = user_light_color
+
+    def __str__(self):
+        """
+        Python's toString equivalent.
+        :return: String representation of User object.
+        """
+        return self.name, self.sound, self.mac, self.light_id, self.light_color
+
+    def __repr__(self):
+        """
+        If within a list, __repr__ is called.
+        :return: String representation of User object.
+        """
+        return self.__str__()

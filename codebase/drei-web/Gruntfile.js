1cd0e83d7b14a8a88f519f5eae5495bb9cb654a5
ifs11030 <ifs11030@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-05-07 07:44:21 +0000

    Renamed index.html to drei.html and corrected links.

--- codebase/drei-web/Gruntfile.js
+++ codebase/drei-web/Gruntfile.js
@@ -163,7 +163,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       app: {
-        src: ['<%= yeoman.app %>/index.html'],
+        src: ['<%= yeoman.app %>/drei.html'],
         ignorePath:  /\.\.\//
       },
       test: {
@@ -200,7 +200,7 @@ module.exports = function (grunt) {
     // concat, minify and revision files. Creates configurations in memory so
     // additional tasks can operate on them
     useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
+      html: '<%= yeoman.app %>/drei.html',
       options: {
         dest: '<%= yeoman.dist %>',
         flow: {
@@ -229,7 +229,7 @@ module.exports = function (grunt) {
     },
 
     // The following *-min tasks will produce minified files in the dist folder
-    // By default, your `index.html`'s <!-- Usemin block --> will take care of
+    // By default, your `drei.html`'s <!-- Usemin block --> will take care of
     // minification. These next options are pre-configured if you do not wish
     // to use the Usemin blocks.
     // cssmin: {

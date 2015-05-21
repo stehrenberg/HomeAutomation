eedea1c547fc766b56b3fd5f128ff3023f1b6356
ifw12385 <ifw12385@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-05-21 12:15:06 +0000

    Multithreaded SoundController

--- codebase/drei/lib/periphery/SoundController.py
+++ codebase/drei/lib/periphery/SoundController.py
@@ -1,85 +1,22 @@
 __author__ = 'Luis'
+
 import platform
 import Const
 
-if platform.machine() == Const.PI_PLATFORM:
-    import pygame
 
-from threading import Thread
-
-
-# TODO Test!!!
 # real implementation on raspberry pi
 if platform.machine() == Const.PI_PLATFORM:
-    class SoundController(Thread):
-        """The sound controller handle the"""
+    import pygame
+    from thread import start_new_thread
 
-        def __init__(self):
-            Thread.__init__(self)
-            self.running = 0
-            pygame.init()
+    def play(song_title):
+        start_new_thread(init_and_play, (song_title,))
 
-        def load(self, song_title):
-            """Load a new song.
-                If a new song is loaded, the actual is stopped."""
+    def init_and_play(song_title):
+        pygame.mixer.init()
+        pygame.mixer.music.load(song_title)
+        pygame.mixer.music.play()
 
-            self.running = 0
-            pygame.mixer.music.load(song_title)
-
-        def play(self):
-            """Play the song, as long as its not stopped."""
-
-            self.start()
-
-        def run(self):
-            self.running = 1
-
-            clock = pygame.time.Clock()
-
-            pygame.mixer.music.play()
-
-            while self.running:
-                clock.tick(60)
-
-        def pause(self):
-            """Pause the actual song."""
-
-            self.running = 0
-
-        def stop(self):
-            """Stop the song and close the instance."""
-
-            self.pause()
-            pygame.quit()
-            self.join(2)
 else:
-        class SoundController(Thread):
-
-            def load(self, song_title):
-                print "SoundController: Loading file " + song_title
-
-            def play(self):
-                print "SoundController: Playing"
-
-            def pause(self):
-                print("SoundController: Pausing")
-
-            def stop(self):
-                print("SoundController: Stopping")
-
-
-if __name__ == '__main__':
-    from time import sleep
-
-    SONG = 'soundFiles/Knight-Rider-Theme-Song.mp3'
-
-    sound = SoundController()
-    sound.load(SONG)
-    sound.play()
-
-    print "Song started"
-
-    sleep(30)
-    sound.stop()
-
-    print "Song stopped"
+    def play(song_title):
+        print "SoundController: Loading file " + song_title

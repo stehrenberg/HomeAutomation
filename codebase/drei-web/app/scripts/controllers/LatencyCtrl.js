dd49428c051cc6b16a3069ec97c4824e3c8fa001
ifs11030 <ifs11030@053ad5a2-b911-4fb6-ae37-d66755214689>
2015-06-25 08:20:41 +0000

    Added performance tests to web-ui.

--- codebase/drei-web/app/scripts/controllers/LatencyCtrl.js
+++ codebase/drei-web/app/scripts/controllers/LatencyCtrl.js
@@ -14,13 +14,16 @@ angular.module('DreiWebApp')
         function ($scope, websocketService) {
 
             $scope.latencyColor = 'red';
+            $scope.performanceColor = 'black';
+
+            $scope.performanceCounter = 0;
 
             $scope.latencyValue = 0;
 
             $scope.changeLatencyColor = function () {
                 var value = $scope.latencyValue;
                 var color;
-                switch(value) {
+                switch (value) {
                     case '1':
                         color = '#00ff00';
                         $scope.latencyColor = color;
@@ -42,11 +45,41 @@ angular.module('DreiWebApp')
                         sendLatencyColor(color);
                         break;
                     default:
-                        color  = '#ff0000';
+                        color = '#ff0000';
                         $scope.latencyColor = color;
                         sendLatencyColor(color);
                 }
             };
+
+            $scope.startPerformanceTest = function () {
+                $scope.performanceCounter = 0;
+                for (var r = 0; r < 256; r+=16) {
+                    for (var g = 0; g < 256; g+=16) {
+                        for (var b = 0; b < 256; b+=16) {
+                            var hex = rgbToHex(r, g, b);
+                            test(hex, r+g+b);
+                        }
+                    }
+                }
+            };
+
+            function test(color, iteration) {
+                setTimeout(function () {
+                    sendLatencyColor(color);
+                    $scope.performanceColor = color;
+                    $scope.performanceCounter++;
+                    $scope.$apply();
+                }, iteration * 10);
+            }
+
+            function componentToHex(c) {
+                var hex = c.toString(16);
+                return hex.length === 1 ? '0' + hex : hex;
+            }
+
+            function rgbToHex(r, g, b) {
+                return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+            }
 
             function sendLatencyColor(color) {
                 websocketService.sendLatencyColor(color);
